eb79a8313456bb02f63e41a6decf5d9787f0a752
NinjaXI <44307836+NinjaXI@users.noreply.github.com>
2024-01-08 19:59:48 +0200

    Created a service to manage Spotify API calls

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,20 @@
-use reqwest::{Client, Response, Error};
-use serde_json::{Value};
+mod spotify;
+
+use reqwest::{Error};
 use config::{Config};
+
+use crate::spotify::spotify_client::SpotifyClient;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let properties = Config::builder().add_source(config::File::with_name("properties")).build().unwrap();
-    let spotify_client_id: &str = &properties.get_string("spotify_client_id").unwrap();
-    let spotify_client_secret: &str = &properties.get_string("spotify_client_secret").unwrap();
-    let token_url: &str = "https://accounts.spotify.com/api/token";
+    let mut spotify_client: SpotifyClient = SpotifyClient::new(properties.get_string("spotify_client_id").unwrap(), properties.get_string("spotify_client_secret").unwrap());
+    spotify_client.get_access_token().await?;
 
-    let client: Client = reqwest::Client::new();
-    let request_json: String = format!("grant_type=client_credentials&client_id={}&client_secret={}", spotify_client_id, spotify_client_secret);
-    let response: Response = client
-        .post(token_url)
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(request_json)
-        .send()
-        .await?;
+    match spotify_client.get_artist("4Z8W4fKeB5YxbusRsdQVPb").await {
+        Ok(artist) => println!("{}", artist),
+        Err(error) => println!("Error occurred while retrieving artist {}", error.to_string())
+    };
 
-    let response_json: Value = serde_json::from_str(&response.text().await?).expect("JSON was not well-formatted");
-    let access_token: &str = response_json["access_token"].as_str().unwrap_or_default();
-
-    let get_response: Response = client.get("https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb").header("Authorization", format!("Bearer {}", access_token)).send().await?;
-    let get_response_json: Value = serde_json::from_str(&get_response.text().await?).expect("JSON was not well-formatted");
-    println!("{}", get_response_json);
     Ok(())
 }
---
+++ src/spotify/spotify_client.rs
@@ -0,0 +1,48 @@
+use reqwest::{Client, Response, Error};
+use serde_json::{Value};
+
+pub struct SpotifyClient {
+    spotify_client_id: String,
+    spotify_client_secret: String,
+    access_token: String,
+    client: Client
+}
+
+impl SpotifyClient {
+
+    pub fn new(spotify_client_id: String, spotify_client_secret: String) -> Self {
+        let access_token: String = "".to_string();
+        let client = Client::new();
+        Self {
+            spotify_client_id,
+            spotify_client_secret,
+            access_token,
+            client
+        }
+    }
+
+    pub async fn get_access_token(&mut self) -> Result<(), Error> {
+        let token_url: &str = "https://accounts.spotify.com/api/token";
+
+        let request_json: String = format!("grant_type=client_credentials&client_id={}&client_secret={}", self.spotify_client_id, self.spotify_client_secret);
+        let response: Response = self.client
+            .post(token_url)
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(request_json)
+            .send()
+            .await?;
+
+
+        let response_json: Value = serde_json::from_str(&response.text().await.unwrap()).expect("JSON was not well-formatted");
+        self.access_token = response_json["access_token"].as_str().unwrap_or_default().to_string();
+
+        Ok(())
+    }
+
+    pub async fn get_artist(&self, artist_id: &str) -> Result<String, Error> {
+        let get_response: Response = self.client.get("https://api.spotify.com/v1/artists/".to_string() + artist_id).header("Authorization", format!("Bearer {}", self.access_token)).send().await?;
+        let get_response_json: Value = serde_json::from_str(&get_response.text().await?).expect("JSON was not well-formatted");
+
+        Ok(get_response_json.to_string())
+    }
+}

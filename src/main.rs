adafeccabcad47d8374e0a43ebef1add0a456a6e
NinjaXI <44307836+NinjaXI@users.noreply.github.com>
2024-01-08 19:59:49 +0200

    Switched over to Implicit Grant authentication flow and make a call to retrieve users saved tracks

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,13 @@
 mod spotify;
 
-use rocket::{get, Rocket, Build, http::Status};
-
 use config::Config;
 use crate::spotify::spotify_client::SpotifyClient;
 
-#[macro_use]
-extern crate rocket;
-
-#[launch]
-fn launch() -> Rocket<Build> {
-    rocket::build().mount("/", routes![get_artist])
-}
-
-// artistId for testing : 4Z8W4fKeB5YxbusRsdQVPb
-#[get("/artist/<artist_id>")]
-async fn get_artist(artist_id: &str) -> Result<(), Status> {
+#[tokio::main]
+async fn main() {
     let properties = Config::builder().add_source(config::File::with_name("properties")).build().unwrap();
-    let mut spotify_client: SpotifyClient = SpotifyClient::new(properties.get_string("spotify_client_id").unwrap(), properties.get_string("spotify_client_secret").unwrap());
-    spotify_client.get_access_token().await.expect("Could not retrieve access token");
+    let mut spotify_client: SpotifyClient = SpotifyClient::new(properties.get_string("spotify_client_id").unwrap());
+    spotify_client.get_implicit_grant_access_token();
 
-    match spotify_client.get_artist(artist_id).await {
-        Ok(artist) => println!("{}", artist),
-        Err(error) => println!("Error occurred while retrieving artist {}", error.to_string())
-    };
-
-    Ok(())
+    println!("{}", spotify_client.get_saved_tracks().await.unwrap());
 }
--- src/spotify/spotify_client.rs
+++ src/spotify/spotify_client.rs
@@ -1,48 +1,128 @@
 use reqwest::{Client, Response, Error};
 use serde_json::Value;
 
+use std::{net::{TcpStream, TcpListener}, io::{Read, Write}};
+
 pub struct SpotifyClient {
     spotify_client_id: String,
-    spotify_client_secret: String,
     access_token: String,
+    token_type: String,
+    expires_in: i32,
     client: Client
 }
 
 impl SpotifyClient {
 
-    pub fn new(spotify_client_id: String, spotify_client_secret: String) -> Self {
+    pub fn new(spotify_client_id: String) -> Self {
         let access_token: String = "".to_string();
+        let token_type: String = "".to_string();
+        let expires_in: i32 = 0;
         let client = Client::new();
+
         Self {
             spotify_client_id,
-            spotify_client_secret,
             access_token,
+            token_type,
+            expires_in,
             client
         }
     }
 
-    pub async fn get_access_token(&mut self) -> Result<(), Error> {
-        let token_url: &str = "https://accounts.spotify.com/api/token";
+    pub fn get_implicit_grant_access_token(&mut self) {
+        let listener: TcpListener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to address");
 
-        let request_json: String = format!("grant_type=client_credentials&client_id={}&client_secret={}", self.spotify_client_id, self.spotify_client_secret);
-        let response: Response = self.client
-            .post(token_url)
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(request_json)
-            .send()
-            .await?;
+        let state: &str = "1234567890123456";
+        let scope: &str = "user-read-private user-read-email user-library-read";
+        let authorization_url: &str = &format!("https://accounts.spotify.com/authorize?response_type=token&client_id={}&scope={}&redirect_uri=http://localhost:8000/callback&state={}", self.spotify_client_id, scope, state);
+        open::that(authorization_url).unwrap();
 
+        let mut running: bool = true;
+        while running {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    stream.read(&mut buffer).expect("Failed to read request");
 
-        let response_json: Value = serde_json::from_str(&response.text().await.unwrap()).expect("JSON was not well-formatted");
-        self.access_token = response_json["access_token"].as_str().unwrap_or_default().to_string();
+                    let request: &str = std::str::from_utf8(&buffer).unwrap();
 
-        Ok(())
+                    let first_line: &str = request.lines().next().unwrap();
+                    let url: &str = first_line.split_whitespace().nth(1).unwrap();
+
+                    if url.contains("finalizeAuthentication") {
+                        self.finalize_implicit_grant(url, state);
+
+                        running = false;
+                    } else {
+                        self.serve_callback(&mut stream);
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Error: {}", e);
+                }
+            }
+        }
     }
 
-    pub async fn get_artist(&self, artist_id: &str) -> Result<String, Error> {
-        let get_response: Response = self.client.get("https://api.spotify.com/v1/artists/".to_string() + artist_id).header("Authorization", format!("Bearer {}", self.access_token)).send().await?;
+    pub async fn get_saved_tracks(&self) -> Result<String, Error> {
+        let get_response: Response = self.client.get("https://api.spotify.com/v1/me/tracks/".to_string()).header("Authorization", format!("{} {}", self.token_type, self.access_token)).send().await?;
         let get_response_json: Value = serde_json::from_str(&get_response.text().await?).expect("JSON was not well-formatted");
 
         Ok(get_response_json.to_string())
     }
+
+    fn serve_callback(&mut self, stream: &mut TcpStream) {
+        let content: String = std::fs::read_to_string("src/html/callback.html").unwrap_or_else(|_| {
+            "Failed to read the HTML file".to_string()
+        });
+
+        let response: String = format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{}",
+            content
+        );
+
+        stream.write(response.as_bytes()).expect("Failed to write response");
+        stream.flush().expect("Failed to flush stream");
+    }
+
+    fn finalize_implicit_grant(&mut self, url: &str, state: &str) {
+        let query_params = url.split("?").nth(1).unwrap().split("&");
+
+        for param in query_params {
+            let mut param_arr = param.split("=");
+            match param_arr.next().unwrap() {
+                "access_token" => {
+                    self.access_token = param_arr.next().unwrap().to_owned();
+                }, "token_type" => {
+                    self.token_type = param_arr.next().unwrap().to_owned();
+                }, "expires_in" => {
+                    self.expires_in = param_arr.next().unwrap().parse().unwrap();
+                }, "state" => {
+                    if state != param_arr.next().unwrap() {
+                        panic!("State does not match")
+                    }
+                }, _ => {
+                    // dont care
+                }
+            }
+        }
+    }
+
+    // no longer used, but left here for reference
+    // pub async fn get_access_token(&mut self) -> Result<(), Error> {
+    //     let token_url: &str = "https://accounts.spotify.com/api/token";
+
+    //     let request_json: String = format!("grant_type=client_credentials&client_id={}&client_secret={}", self.spotify_client_id, self.spotify_client_secret);
+    //     let response: Response = self.client
+    //         .post(token_url)
+    //         .header("Content-Type", "application/x-www-form-urlencoded")
+    //         .body(request_json)
+    //         .send()
+    //         .await?;
+
+
+    //     let response_json: Value = serde_json::from_str(&response.text().await.unwrap()).expect("JSON was not well-formatted");
+    //     self.access_token = response_json["access_token"].as_str().unwrap_or_default().to_string();
+
+    //     Ok(())
+    // }
 }
